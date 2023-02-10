d43b0778b2ff5ba179a1faca33e3a50fde06d3ee
Siren <siren@kernal.eu>
2023-02-10 03:51:58 +0200

    Find zone IDs automatically from zone name

--- main.go
+++ main.go
@@ -26,7 +26,6 @@ type bunnySolver struct {
 
 type bunnyConfig struct {
 	AccessKeySecretRef corev1.SecretKeySelector `json:"apiSecretRef"`
-	ZoneID int64 `json:"zoneId"`
 }
 
 var GroupName = os.Getenv("GROUP_NAME")
@@ -35,7 +34,6 @@ func main() {
 	if GroupName == "" {
 		panic("GROUP_NAME must be specified")
 	}
-
 	cmd.RunWebhookServer(GroupName,
 		&bunnySolver{},
 	)
@@ -46,61 +44,59 @@ func (c *bunnySolver) Name() string {
 }
 
 func (c *bunnySolver) Present(ch *v1alpha1.ChallengeRequest) error {
-	bunnyClient, cfg, err := c.newAPIClient(ch)
+	bunnyClient, err := c.newAPIClient(ch)
 	if err != nil {
 		return err
 	}
-
+	zoneID, err := c.resolveZoneId(bunnyClient, ch.ResolvedZone)
+	if err != nil {
+		return err
+	}
 	recordName := strings.TrimSuffix(strings.TrimSuffix(ch.ResolvedFQDN, ch.ResolvedZone), ".")
-	val, err := c.hasTXTRecord(bunnyClient, recordName, ch.Key, cfg.ZoneID)
+	val, err := c.hasTXTRecord(bunnyClient, recordName, ch.Key, zoneID)
 	if err != nil {
 		return err
 	}
-	// The record is already there.
 	if val != nil {
 		log.Println("TXT record is present, skipping")
 		return nil
 	}
-
 	recordType := 3
-	var ttl int32 = 180
+	var ttl int32 = 120
 	record := &bunny.AddOrUpdateDNSRecordOptions{
 		Type: &recordType,
 		Value: &ch.Key,
 		Name: &recordName,
 		TTL: &ttl,
 	}
-
-	log.Printf("attempting to add record: value=%s, recordname=%s\n", ch.Key, recordName)
-	_, err = bunnyClient.DNSZone.AddDNSRecord(context.Background(), cfg.ZoneID, record)
+	_, err = bunnyClient.DNSZone.AddDNSRecord(context.Background(), zoneID, record)
 	if err != nil {
 		return fmt.Errorf("failed to add TXT record: %s", err.Error())
 	}
-	log.Println("added TXT record")
 	return nil
 }
 
 func (c *bunnySolver) CleanUp(ch *v1alpha1.ChallengeRequest) error {
-	bunnyClient, cfg, err := c.newAPIClient(ch)
+	bunnyClient, err := c.newAPIClient(ch)
 	if err != nil {
 		return err
 	}
-
+	zoneID, err := c.resolveZoneId(bunnyClient, ch.ResolvedZone)
+	if err != nil {
+		return err
+	}
 	recordName := strings.TrimSuffix(strings.TrimSuffix(ch.ResolvedFQDN, ch.ResolvedZone), ".")
-
-	record, err := c.hasTXTRecord(bunnyClient, recordName, ch.Key, cfg.ZoneID)
+	record, err := c.hasTXTRecord(bunnyClient, recordName, ch.Key, zoneID)
 	if err != nil {
 		return fmt.Errorf("failed to get zone records: %v", err)
 	}
 	if record == nil {
 		return nil
 	}
-
-	if err := bunnyClient.DNSZone.DeleteDNSRecord(context.Background(), cfg.ZoneID,
+	if err := bunnyClient.DNSZone.DeleteDNSRecord(context.Background(), zoneID,
 	    *record.ID); err != nil {
 		return fmt.Errorf("failed to delete TXT record: %v", err)
 	}
-
 	return nil
 }
 
@@ -115,8 +111,6 @@ func (c *bunnySolver) Initialize(kubeClientConfig *rest.Config, stopCh <-chan st
 
 func loadConfig(cfgJSON *extapi.JSON) (bunnyConfig, error) {
 	cfg := bunnyConfig{}
-
-	// handle the 'base case' where no configuration has been provided
 	if cfgJSON == nil {
 		return cfg, nil
 	}
@@ -130,12 +124,10 @@ func (c *bunnySolver) getAccessKeyFromSecret(ref corev1.SecretKeySelector, names
 	if ref.Name == "" {
 		return "", fmt.Errorf("undefined access key secret")
 	}
-
 	secret, err := c.client.CoreV1().Secrets(namespace).Get(context.TODO(), ref.Name, metav1.GetOptions{})
 	if err != nil {
 		return "", err
 	}
-
 	accessKey, ok := secret.Data[ref.Key]
 	if !ok {
 		return "", fmt.Errorf("key not found %q in secret '%s/%s'", ref.Key, namespace, ref.Name)
@@ -143,18 +135,16 @@ func (c *bunnySolver) getAccessKeyFromSecret(ref corev1.SecretKeySelector, names
 	return string(accessKey), nil
 }
 
-func (c *bunnySolver) newAPIClient(ch *v1alpha1.ChallengeRequest) (*bunny.Client, bunnyConfig, error) {
+func (c *bunnySolver) newAPIClient(ch *v1alpha1.ChallengeRequest) (*bunny.Client, error) {
 	cfg, err := loadConfig(ch.Config)
 	if err != nil {
-		return nil, cfg, err
+		return nil, err
 	}
-
 	accessKey, err := c.getAccessKeyFromSecret(cfg.AccessKeySecretRef, ch.ResourceNamespace)
 	if err != nil {
-		return nil, cfg, err
+		return nil, err
 	}
-
-	return bunny.NewClient(accessKey), cfg, nil
+	return bunny.NewClient(accessKey), nil
 }
 
 func (c *bunnySolver) hasTXTRecord(client *bunny.Client, name, key string, zoneId int64) (*bunny.DNSRecord, error) {
@@ -168,4 +158,28 @@ func (c *bunnySolver) hasTXTRecord(client *bunny.Client, name, key string, zoneI
 		}
 	}
 	return nil, nil
+}
+
+func (c *bunnySolver) resolveZoneId(client *bunny.Client, zoneName string) (int64, error) {
+	domain := strings.TrimSuffix(zoneName, ".")
+	var i int32
+	for i = 1; ; i++ {
+		zones, err := client.DNSZone.List(context.Background(),
+		    &bunny.PaginationOptions{
+			Page: i,
+			PerPage: 3,
+		})
+		if err != nil {
+			return 0, err
+		}
+		for _, z := range zones.Items {
+			if *z.Domain == domain {
+				return *z.ID, nil
+			}
+		}
+		if *zones.HasMoreItems == false {
+			break
+		}
+	}
+	return 0, fmt.Errorf("failed to get zone id from zone name: %s", zoneName)
 }
